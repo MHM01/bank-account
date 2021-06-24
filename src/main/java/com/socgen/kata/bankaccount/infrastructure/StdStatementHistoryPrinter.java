76a9565bc7d772c13c272c92032db809ec357499
Mohamed Massmoudi <mohamedmassmoudi@MacBook-Air-de-Mohamed.local>
2021-06-24 18:34:38 +0200

    [US-3] Add implementation of standard output statement history printer

--- src/main/java/com/socgen/kata/bankaccount/infrastructure/StdStatementHistoryPrinter.java
+++ src/main/java/com/socgen/kata/bankaccount/infrastructure/StdStatementHistoryPrinter.java
@@ -1,5 +1,7 @@
 package com.socgen.kata.bankaccount.infrastructure;
 
+import java.util.StringJoiner;
+
 import com.socgen.kata.bankaccount.domain.StatementHistoryEntry;
 import com.socgen.kata.bankaccount.domain.port.StatementHistoryPrinter;
 
@@ -12,6 +14,17 @@ public class StdStatementHistoryPrinter implements StatementHistoryPrinter{
 	
 	@Override
 	public void print(StatementHistoryEntry statementHistoryEntry) {
-		throw new UnsupportedOperationException("This operation isn't implemented yet");
+	     System.out.println(formatStatementHistoryEntry(statementHistoryEntry));
+	}
+
+	/**
+	 * @param statementHistoryEntry
+	 * @return
+	 */
+	private static String formatStatementHistoryEntry(StatementHistoryEntry statementHistoryEntry) {
+		return new StringJoiner(" | ").add("Operation='" + statementHistoryEntry.operationType() + "'")
+				.add("Date='" + statementHistoryEntry.operationDate() + "'")
+				.add("Amount='" + statementHistoryEntry.amount().valueInCents() + "'")
+				.add("Balance='" + statementHistoryEntry.balance().valueInCents() + "'").toString();
 	}
 }
